c7ae0b0d750558d81673117191b5e8f52c918d88
liushoukun <liushoukun66@gmail.com>
2024-01-08 23:06:55 +0800

    feat: test

---
+++ lang/en/global.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'enable'  => 'Enable',
+    'disable' => 'Disable',
+    'fields'  => [
+        'id'                    => 'ID',
+        'name'                  => '名称',
+        'username'              => 'username',
+        'email'                 => 'email',
+        'http_path'             => 'http_path',
+        'password'              => 'password',
+        'password_confirmation' => 'password_confirmation',
+        'created_at'            => 'created_at',
+        'updated_at'            => 'updated_at',
+        'permissions'           => 'permissions',
+        'slug'                  => 'slug',
+        'user'                  => 'user',
+        'order'                 => '排序',
+        'ip'                    => 'IP',
+        'method'                => '方法',
+        'uri'                   => 'URI',
+        'roles'                 => '角色',
+        'path'                  => '路径',
+        'input'                 => '输入',
+        'type'                  => '类型',
+    ],
+    'labels'  => [
+        'list'     => '列表',
+        'edit'     => '编辑',
+        'detail'   => '详细',
+        'create'   => '创建',
+        'root'     => '顶级',
+        'scaffold' => '代码生成器',
+    ],
+    'options' => [
+        //
+    ],
+];
