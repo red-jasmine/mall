6d5a7a02418815a5b3a51ab709e7380205a1b0d7
liushoukun <liushoukun66@gmail.com>
2024-07-05 00:12:22 +0800

    refactor: 商品创建功能测试

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['prefix' => 'admin'],function (){
-   // \RedJasmine\Product\UI\Http\Admin\ProductAdminRoute::api();
+   \RedJasmine\Product\UI\Http\Admin\ProductAdminRoute::api();
 
 });
