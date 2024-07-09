8030986f18e7d8a388f8afac1acc5bee97bc23e6
liushoukun <liushoukun66@gmail.com>
2024-07-09 23:40:54 +0800

    feat: 用户接口

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,9 @@ Route::group(['prefix' => 'admin'],function (){
    \RedJasmine\Product\UI\Http\Admin\ProductAdminRoute::api();
 
 });
+
+
+Route::group(['prefix' => 'buyer'],function (){
+    \RedJasmine\Product\UI\Http\Buyer\ProductBuyerRoute::api();
+
+});
