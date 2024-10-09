128e52535cb27cf78c6cb7915a0f0604b26e9f8a
liushoukun <liushoukun66@gmail.com>
2024-10-09 17:36:28 +0800

    添加商品模块数据库迁移文件
    - 新增品牌、类目、库存、附加信息、属性、卖家分类等表结构
    - 定义各表字段、约束及索引
    - 使用配置文件中的表前缀创建表

---
+++ database/migrations/2024_10_09_171026_create_product_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use RedJasmine\Product\Domain\Property\Models\Enums\PropertyStatusEnum;
+use RedJasmine\Product\Domain\Property\Models\Enums\PropertyTypeEnum;
+
+return new class extends Migration {
+    public function up() : void
+    {
+        Schema::create(config('red-jasmine-product.tables.prefix') .'product_properties', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary()->comment('属性ID');
+            $table->unsignedBigInteger('group_id')->nullable()->comment('属性组ID');
+            $table->string('type', 32)->comment(PropertyTypeEnum::comments('类型'));
+            $table->string('name')->comment('名称');
+            $table->string('description')->nullable()->comment('描述');
+            $table->string('unit', 10)->nullable()->comment('单位');
+            $table->unsignedTinyInteger('is_allow_multiple')->default(0)->comment('是否多值');
+            $table->unsignedTinyInteger('is_allow_alias')->default(0)->comment('是否允许别名');
+            $table->unsignedBigInteger('sort')->default(0)->comment('排序');
+            $table->string('status', 32)->comment(PropertyStatusEnum::comments('状态'));
+            $table->nullableMorphs('creator');
+            $table->nullableMorphs('updater');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->comment('商品-属性表');
+        });
+    }
+
+    public function down() : void
+    {
+        Schema::dropIfExists(config('red-jasmine-product.tables.prefix') .'product_properties');
+    }
+};
---
+++ database/migrations/2024_10_09_171032_create_product_skus_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up() : void
+    {
+        Schema::create(config('red-jasmine-product.tables.prefix') .'product_skus', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary()->comment('SKU ID');
+            $table->morphs('owner');
+            $table->unsignedBigInteger('product_id')->default(0)->comment('商品ID');
+            $table->string('properties_name')->nullable()->comment('规格属性名称');
+            $table->string('properties')->nullable()->comment('规格属性值字符串');
+            // SKU 信息
+            $table->decimal('price', 10)->default(0)->comment('销售价');
+            $table->decimal('market_price', 10)->default(0)->comment('市场价');
+            $table->decimal('cost_price', 10)->default(0)->comment('成本价');
+            $table->unsignedBigInteger('sales')->default(0)->comment('销量');
+            // 库存
+            $table->bigInteger('stock')->default(0)->comment('库存');
+            $table->bigInteger('channel_stock')->default(0)->comment('渠道库存');
+            $table->bigInteger('lock_stock')->default(0)->comment('锁定库存');
+            $table->unsignedBigInteger('safety_stock')->default(0)->comment('安全库存');
+            // 信息
+            $table->string('image')->nullable()->comment('主图');
+            $table->string('barcode', 32)->nullable()->comment('条形码');
+            $table->string('outer_id')->nullable()->comment('商家编码');
+            // 状态
+            $table->string('status',32)->comment('状态');
+            // 供应商
+            $table->unsignedBigInteger('supplier_sku_id')->nullable()->comment('供应商 SKU ID');
+            // 操作
+            $table->unsignedBigInteger('version')->default(0)->comment('版本');
+            $table->timestamp('modified_time')->nullable()->comment('修改时间');
+            $table->nullableMorphs('creator');
+            $table->nullableMorphs('updater');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->comment('商品-SKU表');
+            $table->index([ 'product_id', ], 'idx_product');
+            $table->index([ 'owner_id', 'owner_type', ], 'idx_owner');
+        });
+    }
+
+    public function down() : void
+    {
+        Schema::dropIfExists(config('red-jasmine-product.tables.prefix') .'product_skus');
+    }
+};
